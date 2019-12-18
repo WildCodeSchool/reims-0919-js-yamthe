38d43e13f138c7805e314e741de57d2bdc9c6610
Aloxbro <alox51@wanadoo.fr>
2019-12-18 15:46:11 +0100

    handled error 404 and return via html this err

--- back/app.py
+++ back/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "hello World!"
+
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+def create_app(config_filename):
+    app = Flask(__name__)
+    app.register_error_handler(404, page_not_found)
+    return app
+
 
 if __name__ == "__main__":
     app.run(debug=True)
