916ab40462332f7f25b747b52606d37fdbfa8ada
Hugo V <eyleteam@gmail.com>
2019-12-19 10:04:29 +0100

    upload - download

--- back/app.py
+++ back/app.py
@@ -1,26 +1,56 @@
 from flask import Flask
-from flask import render_template
-from flask import request, redirect, url_for
+from flask import render_template, request, redirect, url_for, make_response
 import os
 
 app = Flask(__name__)
 
 
+def transform(text_file_contents):
+    file = open("data.md", "w")
+    file.write(text_file_contents)
+    file.close()
+    os.system("python -m markdown -x codehilite data.md > body.html")
+    secondfile = open("body.html", "r")
+    resultinhtml = secondfile.read()
+    secondfile.close()
+    return resultinhtml
+
+
 @app.route('/')
-def index():
-    return "hello World!"
+def form():
+    return """
+        <html>
+            <body>
+                <h1>Transform a file demo</h1>
+
+                <form action="/transform" method="post" enctype="multipart/form-data">
+                    <input type="file" name="data_file" />
+                    <input type="submit" />
+                </form>
+            </body>
+        </html>
+    """
+
+
+@app.route('/transform', methods=["POST"])
+def transform_view():
+    request_file = request.files['data_file']
+    if not request_file:
+        return "No file"
+
+    file_contents = request_file.stream.read().decode("utf-8")
+
+    result = transform(file_contents)
+
+    response = make_response(result)
+    response.headers["Content-Disposition"] = "attachment; filename=result.html"
+    return response
 
 
 @app.route('/convert', methods=['POST'])
 def login():
     text = request.form['text']
-    file = open("data.md", "w")
-    file.write(text)
-    file.close()
-    os.system("python -m markdown -x codehilite data.md > body.html")
-    secondfile = open("body.html", "r")
-    resultinhtml = secoundfile.read()
-    secoundfile.close()
+    resultinhtml = transform(text)
     return resultinhtml
 
 
