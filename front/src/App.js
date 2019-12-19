4e12110ee84d574b152db32d5603c8b76f28a0ec
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-19 10:21:09 +0100

    Added a component to upload a file to the back-end server

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TextArea from './components/TextArea';
+import FileUpload from './components/FileUpload';
 
 function App() {
   return (
     <div className="App">
       <TextArea />
+      <FileUpload />
     </div>
   );
 }
