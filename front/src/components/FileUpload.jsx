c5bc9930dd9f74374039212393121aa7a01ba1ab
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-20 05:39:02 +0100

    yeet

--- front/src/components/FileUpload.jsx
+++ front/src/components/FileUpload.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import "./FileUpload.css";
+import React from 'react';
+import './FileUpload.css';
 
 const FileUpload = () => {
   return (
-    <form method="post" id="fileUploadForm" enctype="multipart/form-data">
-      <label htmlFor="data_file">Choose a file...</label>
-      <input
-        type="file"
-        name="data_file"
-        id="data_file"
-        style={{ display: "none" }}
-        required
-      />
+    <form method="post" id="fileUploadForm" encType="multipart/form-data">
+      <input type="file" name="data_file" id="data_file" required />
       <button formAction="http://localhost:5000/transform">
         Upload your file
       </button>
